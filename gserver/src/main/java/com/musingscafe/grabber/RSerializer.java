f76f532a8b42a829af36d796f13e373fc5bdcce1
anujyadav1 <yanuj.work@gmail.com>
2016-11-17 11:47:06 +0530

    added reds

---
+++ gserver/src/main/java/com/musingscafe/grabber/RSerializer.java
@@ -0,0 +1,7 @@
+package com.musingscafe.grabber;
+
+/**
+ * Created by ayadav on 11/17/16.
+ */
+public class RSerializer {
+}
---
+++ gserver/src/main/java/com/musingscafe/grabber/channels/Registrar.java
@@ -0,0 +1,7 @@
+package com.musingscafe.grabber.channels;
+
+/**
+ * Created by ayadav on 11/17/16.
+ */
+public class Registrar {
+}
