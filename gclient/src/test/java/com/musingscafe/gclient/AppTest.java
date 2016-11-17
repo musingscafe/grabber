328f5bbf54954de64d1a7d3f72be51939712b9da
anujyadav1 <yanuj.work@gmail.com>
2016-11-17 06:27:20 +0530

    moved sample code for client to AppTest and removed App class. Client would build as a non-executable JAR now.

--- gclient/src/test/java/com/musingscafe/gclient/AppTest.java
+++ gclient/src/test/java/com/musingscafe/gclient/AppTest.java
@@ -1,8 +1,23 @@
 package com.musingscafe.gclient;
 
+import com.musingscafe.grabber.core.Employee;
+import com.musingscafe.grabber.core.channel.Channel;
+import com.musingscafe.grabber.core.channel.ChannelBuilder;
+import com.musingscafe.grabber.core.channel.ChannelConfig;
+import com.musingscafe.grabber.core.consumers.Consumer;
+import com.musingscafe.grabber.core.consumers.PassThroughConsumer;
+import com.musingscafe.grabber.core.message.GrabberMessage;
+import com.musingscafe.grabber.core.message.Tuple;
+import com.musingscafe.newclient.GrabberConnector;
+import com.musingscafe.newclient.NewClient;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+import java.util.UUID;
 
 /**
  * Unit test for simple App.
@@ -33,6 +48,44 @@ public class AppTest
      */
     public void testApp()
     {
-        assertTrue( true );
+        System.out.println( "Hello World!" );
+
+
+        ServerConfig serverConfig = new ServerConfig();
+        serverConfig.setHost("http://localhost");
+        serverConfig.setPort("8084/gmessage");
+
+        ChannelConfig channelConfig = new ChannelConfig("user");
+
+        ChannelBuilder builder = new ChannelBuilder();
+        Channel channel = builder.setChannelIdentifier("user").setConnector(new GrabberConnector(serverConfig))
+                .setConsumers(new ArrayList<Consumer>(){{ add(new PassThroughConsumer());}}).build();
+
+        NewClient.open(serverConfig, new ArrayList<Channel>(){{add(channel);}}, NewClient.DB_PATH);
+
+        send(channel);
+
+//        int value = 0;
+//        while (value != 2) {
+//            Scanner scanner = new Scanner(System.in);
+//            value = scanner.nextInt();
+//            for (int i = 0; i < value; i++) {
+//                send(channel);
+//            }
+//        }
+    }
+
+    private static void send(Channel grabberChannel) {
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setName(UUID.randomUUID().toString());
+
+        LinkedHashMap<String, Object> fields = new LinkedHashMap<>();
+        fields.put("employee", employee);
+        Tuple tuple = new Tuple(fields);
+
+        GrabberMessage message = new GrabberMessage();
+        message.setContent(tuple);
+        grabberChannel.write(message);
     }
 }
